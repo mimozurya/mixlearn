af967df1ae8be3f79d41719d6ca152bc5ed33a27
mimozurya <egor_sidorenko007@mail.ru>
2023-11-23 00:26:48 +0500

    регистрация

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Registration.scss";
+import { useState, useEffect } from "react";
+import Homepage from "../Homepage";
 
 function Registration() {
+    const [user, setUser] = useState([]);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [canGo, setCanGo] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            const userResponce = await axios.get(
+                "https://655e414a9f1e1093c59acfec.mockapi.io/user"
+            );
+            setUser(userResponce.data);
+        }
+
+        fetchData();
+    }, []);
+
+    const checkUserData = () => {
+        user.some(
+            (human) =>
+                String(human.username) === String(username) &&
+                String(human.password) === String(password)
+        ) && setCanGo(true);
+    };
+
     return (
         <section className="registration-container">
             <div className="container d-flex justify-center align-center">
@@ -12,15 +39,34 @@ function Registration() {
                     <div className="registration-buttons d-flex flex-column justify-center align-center">
                         <div className="icon username d-flex align-center">
                             <img src="../../src/assets/registration/user.svg" alt="user" />
-                            <input placeholder="Имя_пользователя" type="text" />
+                            <input
+                                placeholder="Имя_пользователя"
+                                type="text"
+                                onChange={(event) => setUsername(event.target.value)}
+                                value={username}
+                            />
                         </div>
                         <div className="icon password d-flex align-center">
                             <img src="../../src/assets/registration/password.svg" alt="password" />
-                            <input placeholder="Пароль" type="text" />
+                            <input
+                                placeholder="Пароль"
+                                type="password"
+                                onChange={(event) => setPassword(event.target.value)}
+                                value={password}
+                            />
                         </div>
-                        <Link to="/homepage" className="clean">
-                            <button>Войти</button>
-                        </Link>
+                        <div className="d-flex justify-between">
+                            {canGo ? (
+                                <Link to="/homepage" className="clean">
+                                    <button>Удачи!</button>
+                                </Link>
+                            ) : (
+                                <>
+                                    <button onClick={checkUserData}>Войти</button>
+                                    <button>Зарегистрироваться</button>
+                                </>
+                            )}
+                        </div>
                     </div>
                     <h2>Зарегистрироваться как:</h2>
                     <div className="registration-user d-flex justify-center align-center">
